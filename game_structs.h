20781a6dda7b3f6ad4a977a028b9ad8fbacb8802
g14-bt <bryce.towns@gmail.com>
2014-02-11 23:48:44 -0800

    plz finally

--- game_structs.h
+++ game_structs.h
@@ -89,12 +89,13 @@ typedef struct Enemy
         // vga info like the boxes that make up the image and colour
         int colour;
 
-        //Status effect, 0 for none, 1 for slow, 2 for poison
-        int status;
+        //Status effect, 0 for none, 1 for yes
+        int poison;
+        int slow;
 
         //Status effect countdown, when hit with ice or slow, set at a value, counts down until effect is gone
-        int statusCountdown;
-
+        int poisonCountdown;
+        int slowCountdown;
 
         //pointer to the next and previous enemies
         struct Enemy* next;
